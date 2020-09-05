e55861c604da2d9e07b8fe88e18bf703b4e2cf17
wangxuanran12 <783158377@qq.com>
2020-09-05 17:25:50 +0800

    创建

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>现在是 {this.props.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+function tick() {
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('example')
+    );
+}
+
+setInterval(tick, 1000);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
